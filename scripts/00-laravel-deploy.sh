78db8dc1001b921b0087d7d8a412aae3dad9a56f
misterk <kantegilchrist@gmail.com>
2025-09-08 11:57:53 +0100

    Fix: add npm install in scripts

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 echo "Running composer"
 composer install --no-dev --working-dir=/var/www/html
+npm install
 
 echo "Caching config..."
 php artisan config:cache
@@ -10,3 +11,5 @@ php artisan route:cache
 
 echo "Running migrations..."
 php artisan migrate --force
+
+npm run dev
